795f6be4d0fc97eb2783efaa7cca2db45baf8c18
Miitri Raatikainen <raatikaine@gmail.com>
2017-03-26 14:12:09 +0300

    Display remaining minutes to departure if less than 10.

--- src/departureslist/time.js
+++ src/departureslist/time.js
@@ -4,6 +4,13 @@
  * @param {number} num
  */
 const padNumber = num => (('' + num).length < 2 ? '0' + num : num);
+/**
+ * Get time as string containing hours and minutes separated by a colon
+ * @private
+ * @param {Date} date
+ * @returns {string}
+ */
+const getTimeAsText = date => `${padNumber(date.getHours())}:${padNumber(date.getMinutes())}`;
 
 /**
  * Factory function for Time component
@@ -15,13 +22,20 @@ export default Inferno =>
  * Component for displaying time in human readable form
  * @constructs Time
  * @param {Object} props
- * @param {number} props.time
+ * @param {number} props.time Time in seconds
  * @return {Time}
  */
 ({ time }) => {
+    const now = Date.now();
     const date = new Date(time * 1000);
-    const hours = padNumber(date.getHours());
-    const minutes = padNumber(date.getMinutes());
+    const timeLeftInMins = Math.floor(((date - now) / 1000) / 60);
+    let timeText = '';
 
-    return <span>{`${hours}:${minutes}`}</span>;
+    if (date > now && timeLeftInMins < 10) {
+        timeText = timeLeftInMins < 1 ? 'Now' : `${timeLeftInMins} min`;
+    } else {
+        timeText = getTimeAsText(date);
+    }
+
+    return <span>{timeText}</span>;
 };
