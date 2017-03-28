da558609518029403f2305cc2efb9c73dbc6d4b1
Miitri Raatikainen <raatikaine@gmail.com>
2017-03-28 18:10:05 +0300

    FindFrom utility function.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -10,3 +10,27 @@ export const getDepartureTime = d => (d.realtime ? d.realtimeDeparture : d.sched
  * @returns {number}
  */
 export const getNowInSeconds = () => Math.floor(new Date().getTime() / 1000);
+
+/**
+ * Find a value or some of values from list
+ * @param {Array} list
+ * @param {string} [prop] If defined will compare subject[prop] with entry[prop]
+ * @returns {Function}
+ */
+export const findFrom = (list = [], prop) => {
+    // get entity[prop] if prop defined else return entity
+    const getProp = prop ? e => e[prop] : e => e;
+    // array comparator
+    const compareArray = subject => e => subject.indexOf(getProp(e)) > -1;
+    // single comparator
+    const compareProp = subject => e => getProp(e) === subject;
+    /**
+     * @param {*} subject
+     * @returns {*}
+     */
+    return subject => {
+        const subjectValue = getProp(subject);
+        const comparator = Array.isArray(subject) ? compareArray : compareProp;
+        return list.find(comparator(subjectValue));
+    }
+}
