0be251277487d62451a76fa9f3018850b4c34b73
Miitri Raatikainen <raatikaine@gmail.com>
2017-04-21 15:25:48 +0300

    Do not use Proxy for compatibility reasons.

--- src/utils/fputils.js
+++ src/utils/fputils.js
@@ -12,25 +12,23 @@ const rightCurried = ['assign', 'or'];
 /**
  * Proxy for 1-liners utility functions. Auto curries all functions except ones
  * defined in unCurried list.
- * @type {Proxy}
+ * @type {Object}
  */
-export default new Proxy(oneliners, {
-    /**
-     * Getter for utility functions
-     * @param {Object} obj
-     * @param {string} prop Fn name
-     * @returns {Function} Curried function
-     */
-    get(obj, prop) {
-        const fn = obj[prop];
-        if (!fn) throw new Error(`IllegalArgumentException: No such function found ${prop}`);
-        if (unCurried.indexOf(prop) > -1) return fn;
-        // some fns should be curried right (such as assign)
-        if (rightCurried.indexOf(prop) > -1) return oneliners.curryRight(fn);
-        const arity = fn.length;
+export default Object.keys(oneliners).reduce((obj, key) => {
+    const orig = oneliners[key];
+    const arity = orig.length;
+    let fn;
+
+    if (unCurried.indexOf(key) > -1 || arity < 2) {
+        fn = orig;
+    // some fn should be curried right (such as assign)
+    } else if (rightCurried.indexOf(key) > -1) {
+        fn = oneliners.curryRight(orig);
+    } else {
         // do not curry if fn's arity is one or less
-        if (arity < 2) return fn;
         const curry = oneliners.reduce(oneliners.compose, [new Array(arity).map(() => curry)]);
-        return oneliners.curry(fn);
+        fn = oneliners.curry(orig);
     }
-});
+    obj[key] = fn;
+    return obj;
+}, {});
