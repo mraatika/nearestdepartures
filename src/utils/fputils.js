5f20990866fb080e9862f119065d3d01cf363fc0
Miitri Raatikainen <raatikaine@gmail.com>
2017-04-24 11:49:46 +0300

    Fixed multi curry.

--- src/utils/fputils.js
+++ src/utils/fputils.js
@@ -10,6 +10,12 @@ const unCurried = ['curry', 'compose', 'composeAll', 'ifThenElse', 'pipe', 'pipe
 const rightCurried = ['assign', 'or'];
 
 /**
+ * Compose a curry function to curry multiple times (e.g sum(1)(2)(3))
+ * @param {number} arity
+ */
+const curryMany = (arity) => oneliners.reduce(oneliners.compose, [...new Array(arity - 1)].map(() => oneliners.curry));
+
+/**
  * Proxy for 1-liners utility functions. Auto curries all functions except ones
  * defined in unCurried list.
  * @type {Object}
@@ -19,15 +25,17 @@ export default Object.keys(oneliners).reduce((obj, key) => {
     const arity = orig.length;
     let fn;
 
-    if (unCurried.indexOf(key) > -1 || arity < 2) {
+    // do not curry if
+    // a) property is not a function
+    // b) property is listed in list of fns that should not be curried
+    // c) fn's arity is one or less
+    if (typeof orig !== 'function' || unCurried.indexOf(key) > -1 || arity < 2) {
         fn = orig;
     // some fn should be curried right (such as assign)
     } else if (rightCurried.indexOf(key) > -1) {
         fn = oneliners.curryRight(orig);
     } else {
-        // do not curry if fn's arity is one or less
-        const curry = oneliners.reduce(oneliners.compose, [new Array(arity).map(() => curry)]);
-        fn = oneliners.curry(orig);
+        fn = curryMany(arity)(orig);
     }
     obj[key] = fn;
     return obj;
