aa12562a641c58943b47c02b3865fcb9fe8d2c91
miitri raatikainen <miitri.raatikainen@siili.com>
2017-11-10 11:55:43 +0200

    Surround fetch calls with try-catch to throw an error with a human readable error message instead of "Failed to fetch"

--- src/services/departuresservice.js
+++ src/services/departuresservice.js
@@ -7,125 +7,139 @@ import { getNowInSeconds } from '../utils/utils';
 /** @module DeparturesService */
 
 /**
- * Limit results by time (2h in seconds)
- * @private
- * @type {number}
- */
+* Limit results by time (2h in seconds)
+* @private
+* @type {number}
+*/
 const TIME_RANGE = 3 * 60 * 60;
 /**
- * Number of stoptimes per route to fetch
- * @private
- * @type {number}
- */
+* Number of stoptimes per route to fetch
+* @private
+* @type {number}
+*/
 const NUMBER_OF_DEPARTURES_PER_ROUTE = 2;
 /**
- * Max number of results to fetch
- * @private
- * @type {number}
- */
+* Max number of results to fetch
+* @private
+* @type {number}
+*/
 const MAX_RESULTS = 20;
 
 /**
- * Form graphql query for request body
- * @private
- * @param {Object} props
- * @param {number} props.latitude
- * @param {number} props.longitude
- * @param {number} props.startTime
- * @param {string[]} props.vehicleTypes
- * @returns {Object}
- */
+* Form graphql query for request body
+* @private
+* @param {Object} props
+* @param {number} props.latitude
+* @param {number} props.longitude
+* @param {number} props.startTime
+* @param {string[]} props.vehicleTypes
+* @returns {Object}
+*/
 function formRequestBody({ latitude, longitude, startTime, vehicleTypes } = {}) {
-    return {
-        query: query,
-        variables: {
-            latitude,
-            longitude,
-            vehicleTypes,
-            timeRange: TIME_RANGE,
-            departuresCount: NUMBER_OF_DEPARTURES_PER_ROUTE,
-            maxResults: MAX_RESULTS,
-        },
-    };
+  return {
+    query: query,
+    variables: {
+      latitude,
+      longitude,
+      vehicleTypes,
+      timeRange: TIME_RANGE,
+      departuresCount: NUMBER_OF_DEPARTURES_PER_ROUTE,
+      maxResults: MAX_RESULTS,
+    },
+  };
 }
 
 /**
- * Fetch nearest departures from digitransit's public api
- * @async
- * @param {Object} location
- * @param {number} location.latitude
- * @param {number} location.longitude
- * @returns {Promise}
- */
+* Fetch nearest departures from digitransit's public api
+* @async
+* @param {Object} location
+* @param {number} location.latitude
+* @param {number} location.longitude
+* @returns {Promise}
+*/
 export async function fetchDepartures(location = {}, filters = {}) {
-    const { latitude = 60.189425, longitude = 24.951884 } = location;
-    const { vehicleTypes } = filters;
-    const reqBody = formRequestBody({ latitude, longitude, vehicleTypes });
+  const { latitude = 60.189425, longitude = 24.951884 } = location;
+  const { vehicleTypes } = filters;
+  const reqBody = formRequestBody({ latitude, longitude, vehicleTypes });
+  let response;
 
-    const response = await fetch('https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(reqBody),
+  try {
+    response = await fetch('https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(reqBody),
     });
+  } catch (e) {
+    throw new Error('Lähtöjen haku epäonnisui: Palveluun ei saatu yhteyttä');
+  }
 
-    if (!response.ok) throw new Error('Palvelu palautti virheen');
+  if (!response.ok) {
+    throw new Error('Lähtöjen haku epäonnisui: Palvelu palautti virheen');
+  }
 
-    const data = await response.json();
+  const data = await response.json();
 
-    return parseFetchResponse(data);
+  return parseFetchResponse(data);
 }
 
 /**
- * Form body for batch request
- * @private
- * @param {Object} props
- * @param {string} props.id
- * @returns {Object}
- */
+* Form body for batch request
+* @private
+* @param {Object} props
+* @param {string} props.id
+* @returns {Object}
+*/
 function formBatchRequestBody({ id }) {
-    const startTime = getNowInSeconds();
+  const startTime = getNowInSeconds();
 
-    return {
-        query: batchQuery,
-        variables: {
-            id,
-            startTime,
-            departuresCount: NUMBER_OF_DEPARTURES_PER_ROUTE,
-        }
-    };
+  return {
+    query: batchQuery,
+    variables: {
+      id,
+      startTime,
+      departuresCount: NUMBER_OF_DEPARTURES_PER_ROUTE,
+    }
+  };
 }
 
 /**
- * Parse batch response data
- * @private
- * @param {Object[]} data
- * @returns {Function}
- */
+* Parse batch response data
+* @private
+* @param {Object[]} data
+* @returns {Function}
+*/
 const parseBatchResponse = fputils.flatMap((data) => {
-    const { id: nodeId, stoptimes } = data.payload.data.node;
-    if (!stoptimes) return [];
-    return stoptimes.map((stoptime) => Object.assign({ nodeId }, formStoptimeData(stoptime)));
+  const { id: nodeId, stoptimes } = data.payload.data.node;
+  if (!stoptimes) return [];
+  return stoptimes.map((stoptime) => Object.assign({ nodeId }, formStoptimeData(stoptime)));
 });
 
 /**
- * Batch updated departures
- * @param {Object[]} [departures=[]] Departures to batch
- * @returns {Object[]}
- */
+* Batch updated departures
+* @param {Object[]} [departures=[]] Departures to batch
+* @returns {Object[]}
+*/
 export async function batchDepartures(departures = []) {
-    if (!departures.length) return departures;
+  if (!departures.length) return departures;
 
-    const query = departures.map(d => formBatchRequestBody({ id: d.nodeId }));
+  const query = departures.map(d => formBatchRequestBody({ id: d.nodeId }));
+  let response;
 
-    const response = await fetch('https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql/batch', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(query),
+  try {
+    response = await fetch('https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql/batch', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(query),
     });
+  } catch (e) {
+    throw new Error('Lähtöjen päivitys epäonnistui: Palveluun ei saatu yhteyttä');
+  }
 
-    if (!response.ok) throw new Error('Palvelu palautti virheen');
+  if (!response.ok) {
+    throw new Error('Lähtöjen päivitys epäonnistui: Palvelu palautti virheen');
+  }
 
-    const data = await response.json();
+  const data = await response.json();
 
-    return parseBatchResponse(data);
+  return parseBatchResponse(data);
 }
