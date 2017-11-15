d54c792c6a1cfd48faa52c17a81498e7d0eea6ec
miitri raatikainen <miitri.raatikainen@siili.com>
2017-11-15 14:19:28 +0200

    Add pwa server worker that was lost in refactor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { render } from 'inferno';
 import App from './components/app/app';
 import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 
 render(<App />, document.getElementById('app'));
+
+registerServiceWorker();
