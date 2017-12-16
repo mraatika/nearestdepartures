5885ed4aaef87977ef3895d7d0820e12866f61ec
miitri raatikainen <miitri.raatikainen@siili.com>
2017-12-16 16:31:02 +0200

    Separate logic from Favourites to a container component

--- src/components/favourites/model.js
+++ src/components/favourites/model.js
@@ -1,1 +1,21 @@
+import { find } from '../../utils/utils';
+import fputils from '../../utils/fputils';
+
+/**
+ * Check if two addresses have equal labels
+ * @param {object} a
+ * @param {object} b
+ * @return {boolean}
+ */
 export const areLocationsEqual = (a = {}, b = {}) => a.label === b.label;
+
+const curriedAreLocationsEqual = fputils.curry(areLocationsEqual);
+
+/**
+ * Check if an adress is in the list of favoured addresses
+ * @param {object} address
+ * @param {object[]} favourites
+ * @return {boolean}
+ */
+export const isLocationFavoured = (address, favourites) =>
+  !!(address && find(curriedAreLocationsEqual(address))(favourites));
