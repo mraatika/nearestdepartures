972e909b42f7414b9ef929ba06b468bd4bf6bdde
miitri raatikainen <miitri.raatikainen@siili.com>
2018-02-01 23:05:13 +0200

    Mock localStorage for tests

--- config/testsetup.js
+++ config/testsetup.js
@@ -1,1 +1,20 @@
 global.fetch = require('jest-fetch-mock');
+
+// mock localStorage for tests
+const localStorageMock = (() => {
+  let store = {};
+
+  return {
+      getItem(key) {
+        return store[key] || null;
+      },
+      setItem(key, value) {
+        store[key] = value.toString();
+      },
+      clear() {
+        store = {};
+      }
+  };
+})();
+
+global.localStorage = localStorageMock;
