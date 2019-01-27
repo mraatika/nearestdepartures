187d458ee5eaaea4602d3e3f67ae5d415f1dec4b
miitri raatikainen <miitri.raatikainen@siili.com>
2019-01-27 14:00:31 +0200

    Remove animation from departure row additional info toggling Render the section only when it is clicked open.

--- src/components/departureslist/departurerow.js
+++ src/components/departureslist/departurerow.js
@@ -1,4 +1,4 @@
-import { Component, linkEvent } from 'inferno';
+import { linkEvent, forwardRef } from 'inferno';
 import Time from '../time';
 import DisruptionAlert from './disruptionalert';
 import RouteIdentifier from './routeidentifier';
@@ -53,11 +53,13 @@ export default ({
       <div class={`time${realtime ? ' realtime' : ''}`}>
         <Time time={realtimeDeparture} />
       </div>
+
       <div class="routename">
         <ExternalLink href={routeUrl}>
           <RouteIdentifier vehicleType={vehicleType} routeName={routeName} />
         </ExternalLink>
       </div>
+
       <div class="destination">
         {!!(disruptions.length) &&
           <span
@@ -70,21 +72,28 @@ export default ({
           </span>}
         {destination}
       </div>
-      <div class="distance"><Distance distance={distance} /></div>
+
+      <div class="distance">
+        <Distance distance={distance} />
+      </div>
     </div>
-    <DepartureRowAdditionalContent {...{
-      id,
-      realtime,
-      realtimeDeparture,
-      scheduledDeparture,
-      isToggled,
-      stopUrl,
-      stopName,
-      stopCode,
-      stopDescription,
-      disruptions,
-      onRowToggle,
-    }}/>
+
+    {isToggled &&
+      <DepartureRowAdditionalContent
+        ref={content => content && requestFocus(content)}
+        {...{
+          id,
+          realtime,
+          realtimeDeparture,
+          scheduledDeparture,
+          stopUrl,
+          stopName,
+          stopCode,
+          stopDescription,
+          disruptions,
+          onRowToggle,
+        }}
+      />}
   </li>;
 
 /**
@@ -93,75 +102,59 @@ export default ({
  * @constructs DepartureRowAdditionalContent
  * @param {object} props
  */
-class DepartureRowAdditionalContent extends Component {
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.isToggled && this.props.isToggled !== nextProps.isToggled) {
-      requestFocus(this.additionalContent);
-    }
-  }
-
-  render () {
-    const {
-      id,
-      realtime,
-      realtimeDeparture,
-      scheduledDeparture,
-      isToggled,
-      stopUrl,
-      stopName,
-      stopCode,
-      stopDescription,
-      disruptions,
-      onRowToggle,
-    } = this.props;
-
-    return (
-      <div
-        id={`departure-${id}`}
-        class={`departures-list-row-additional-info${isToggled ? ' visible' : ''}`}
-        aria-hidden={!isToggled}
-        tabIndex={isToggled ? 0 : -1}
-        ref={e => this.additionalContent = e}
-        onKeyUp={keyPressHandler([27], onRowToggle, id)}
-      >
-        <div class="departures-list-row-additional-info-content">
-          <div class="departure-additional-info-content-block">
-            <Icon type="clock" />
-            <div>
-              {realtime && <div class="realtime bold " title="Arvioitu reaaliaikainen lähtöaika pysäkiltä">
-                <Time time={realtimeDeparture} actualTime={true} /> (arvioitu)
-              </div>}
-              <div class="scheduled-departure" title="Aikataulun mukainen lähtöaika pysäkiltä">
-                <Time time={scheduledDeparture} actualTime={true} /> (aikataulu)
-              </div>
-            </div>
+const DepartureRowAdditionalContent = forwardRef(({
+  id,
+  realtime,
+  realtimeDeparture,
+  scheduledDeparture,
+  stopUrl,
+  stopName,
+  stopCode,
+  stopDescription,
+  disruptions,
+  onRowToggle,
+}, ref) =>
+  <section
+    id={`departure-${id}`}
+    class="departures-list-row-additional-info"
+    tabIndex="0"
+    ref={ref}
+    onKeyUp={keyPressHandler([27], onRowToggle, id)}
+  >
+    <div class="departures-list-row-additional-info-content">
+      <div class="departure-additional-info-content-block">
+        <Icon type="clock" />
+        <div>
+          {realtime && <div class="realtime bold " title="Arvioitu reaaliaikainen lähtöaika pysäkiltä">
+            <Time time={realtimeDeparture} actualTime={true} /> (arvioitu)
+          </div>}
+          <div class="scheduled-departure" title="Aikataulun mukainen lähtöaika pysäkiltä">
+            <Time time={scheduledDeparture} actualTime={true} /> (aikataulu)
           </div>
-          <div class="departure-additional-info-content-block">
-            <Icon type="bus-stop" />
-            <div>
-              <ExternalLink
-                class="bold departure-stop-name"
-                tabIndex={isToggled ? '0' : '-1'}
-                href={stopUrl}
-                title="Näytä pysäkin tiedot Reittioppaassa"
-                text={stopName}
-              />
-              <div>
-                <div class="departure-stop-code">{stopCode}</div>
-                <span class="departure-stop-description">{stopDescription}</span>
-              </div>
-            </div>
-          </div>
-          {!!disruptions.length && <DisruptionAlert disruptions={disruptions} />}
         </div>
-
-        <button
-          class="departures-list-row-additional-info-close text-only-button sr-only sr-only-focusable"
-          onClick={linkEvent(id, onRowToggle)}>
-            Sulje
-        </button>
       </div>
-    );
-  }
-}
+      <div class="departure-additional-info-content-block">
+        <Icon type="bus-stop" />
+        <div>
+          <ExternalLink
+            class="bold departure-stop-name"
+            href={stopUrl}
+            title="Näytä pysäkin tiedot Reittioppaassa"
+            text={stopName}
+          />
+          <div>
+            <div class="departure-stop-code">{stopCode}</div>
+            <span class="departure-stop-description">{stopDescription}</span>
+          </div>
+        </div>
+      </div>
+      {!!disruptions.length && <DisruptionAlert disruptions={disruptions} />}
+    </div>
+
+    <button
+      class="departures-list-row-additional-info-close text-only-button sr-only sr-only-focusable"
+      onClick={linkEvent(id, onRowToggle)}>
+        Sulje
+    </button>
+  </section>
+);
