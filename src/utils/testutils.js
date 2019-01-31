3db8c307d2922563142256ec0698663aef425b73
miitri raatikainen <miitri.raatikainen@siili.com>
2019-01-31 10:57:44 +0200

    Shallow clone body before rendering  component into document Removes event listeners etc. and prevents stuff from leaking to another tests

--- src/utils/testutils.js
+++ src/utils/testutils.js
@@ -1,6 +1,7 @@
 import { render } from 'inferno';
 
 export const renderIntoDocument = Component => {
+  document.body = document.body.cloneNode();
   const node = document.createElement('div');
   document.body.appendChild(node);
   return render(Component, node);
