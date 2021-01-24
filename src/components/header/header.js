ce0b98646b49372d56375bca4232cdea5f9bfca6
Miitri Raatikainen <raatikaine@gmail.com>
2021-01-24 13:28:43 +0200

    fix: Prevent layout shift Specify image/image container height and width to prevent layout shifting. Resized images for narrow and wide screens.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,4 @@
 import './header.css';
-import logo from './julkisilla_logo.png';
 import Favourites from '../favourites';
 
 /**
@@ -9,23 +8,27 @@ import Favourites from '../favourites';
  * @param {object} props.address
  * @param {function} props.selectLocation
  */
-const Header = ({ address, selectLocation }) => (
+const Header = ({ address, selectLocation }) => (
   <header class="bg-bus color-white space-xs space-clear-rl">
     <div class="header-content flex-row flex-align-center centering-margin max-content-width space-s space-clear-tb">
       <div class="flex-full no-wrap">
-        <h1 class="space-s space-clear-rl">
-          <img class="app-logo" src={logo} alt="" />
-          <span class="app-name font-heading lowercase">julkisilla.info</span>
+        <h1 class="flex-full no-wrap flex-row flex-align-center space-s space-clear-rl">
+          <div class="app-logo-container">
+            <img
+              class="app-logo"
+              src="icons/app-logo-40.png"
+              srcset="icons/app-logo-60.png 60w"
+              alt=""
+            />
+          </div>
+          <div class="app-name font-heading lowercase">julkisilla.info</div>
         </h1>
         <p class="app-description text-s line-height-s space-l space-keep-b">
           Löydä lähimmät julkisen liikenteen lähdöt helposti
         </p>
       </div>
 
-      <Favourites
-        address={address}
-        selectLocation={selectLocation}
-      />
+      <Favourites address={address} selectLocation={selectLocation} />
     </div>
   </header>
 );
