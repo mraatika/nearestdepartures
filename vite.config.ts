1a8f0f7ddf6c1a65669bc4a6a668d3431e8816a8
mraatika <raatikaine@gmail.com>
2022-10-23 13:30:50 +0300

    feat: PWA Service worker with vite-pwa. Add maskable icon. Add apple-touch-icon to index.html.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig, type PluginOption } from 'vite';
-import Icons from 'unplugin-icons/vite';
-import { visualizer } from 'rollup-plugin-visualizer';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import * as path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
+import Icons from 'unplugin-icons/vite';
+import { defineConfig, type PluginOption } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,6 +25,7 @@ export default defineConfig({
     Icons({
       compiler: 'svelte',
     }),
+    VitePWA(),
     visualizer() as PluginOption,
   ],
 });
