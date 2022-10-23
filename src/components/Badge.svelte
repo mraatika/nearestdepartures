33939e5cda7a378bc9978bcc1af28a63f3021b5c
mraatika <raatikaine@gmail.com>
2022-10-23 12:17:13 +0300

    feat: Add link to github to the footer

---
+++ src/components/Badge.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let bgColor: string;
+</script>
+
+<span class="{`space-xs flex-row bg-${bgColor}`}">
+  <slot />
+</span>
+
+<style>
+  span {
+    border-radius: 50%;
+  }
+</style>
--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,26 +1,40 @@
 <script lang="ts">
+  import GithubIcon from '~icons/lucide/github';
   import ExternalLink from './ExternalLink.svelte';
   import { lastUpdatedStore } from '@/stores';
   import { toTimeStringWithSeconds } from '@/util';
+  import Badge from '@/components/Badge.svelte';
 </script>
 
 <footer class="full-width text-s color-white bg-bus-dark space-s">
-  <div class="footer-content max-content-width centering-margin">
-    <p class="footer-app-name font-heading space-xs space-keep-b">
-      {`Julkisilla.info v${import.meta.env.VITE_APP_VERSION}`}
-    </p>
-    <p class="line-height-m">
-      {#if $lastUpdatedStore}
-        Lähdöt päivitetty
-        <span class="italic">
-          {`${toTimeStringWithSeconds($lastUpdatedStore)} / `}
-        </span>
-      {/if}
+  <div class="max-content-width centering-margin flex-row flex-space-between">
+    <div>
+      <p class="font-heading space-xs space-keep-b">
+        {`Julkisilla.info v${import.meta.env.VITE_APP_VERSION}`}
+      </p>
 
-      Lähtöjen tiedot ovat HSL:n tarjoamaa
-      <ExternalLink href="https://digitransit.fi/" class="underline">
-        avointa dataa
-      </ExternalLink>.
-    </p>
+      <p class="line-height-m">
+        {#if $lastUpdatedStore}
+          Lähdöt päivitetty
+          <span class="italic">
+            {`${toTimeStringWithSeconds($lastUpdatedStore)} / `}
+          </span>
+        {/if}
+        Lähtöjen tiedot ovat HSL:n tarjoamaa
+        <ExternalLink href="https://digitransit.fi/" class="underline">
+          avointa dataa
+        </ExternalLink>.
+      </p>
+    </div>
+    <div>
+      <ExternalLink
+        href="https://github.com/mraatika/nearestdepartures"
+        showIcon="{false}"
+      >
+        <Badge bgColor="black">
+          <GithubIcon />
+        </Badge>
+      </ExternalLink>
+    </div>
   </div>
 </footer>
