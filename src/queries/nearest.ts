d068206c5fd746eec71bf5b4cc3a34a94529ed40
mraatika <raatikaine@gmail.com>
2022-10-23 11:03:03 +0300

    refactor: reindent nearest query

--- src/queries/nearest.ts
+++ src/queries/nearest.ts
@@ -3,45 +3,45 @@
  */
 export default `
  query Nearest($latitude: Float!, $longitude: Float!, $maxResults: Int, $timeRange:Int, $departuresCount:Int, $maxDistance:Int) {
-     nearest(lat: $latitude, lon: $longitude, maxResults: $maxResults, maxDistance: $maxDistance, filterByPlaceTypes: DEPARTURE_ROW) {
-         edges {
-             node {
-                 id
-                 distance
-                 place {
-                     id
-                     ... on DepartureRow {
-                         stop {
-                           gtfsId
-                           code
-                           name
-                           desc
-                         }
-                         stoptimes(timeRange: $timeRange, numberOfDepartures: $departuresCount, omitNonPickups: true) {
-                             serviceDay
-                             scheduledDeparture
-                             realtimeDeparture
-                             realtimeState
-                             realtime
-                             headsign
-                             trip {
-                                 id
-                             }
-                         }
-                         pattern {
-                             route {
-                                 gtfsId
-                                 shortName
-                                 longName
-                                 mode
-                             }
-                             code
-                             headsign
-                         }
-                     }
-                 }
-             }
-         }
-     }
- }
-}`;
+    nearest(lat: $latitude, lon: $longitude, maxResults: $maxResults, maxDistance: $maxDistance, filterByPlaceTypes: DEPARTURE_ROW) {
+      edges {
+        node {
+          id
+          distance
+          place {
+            id
+            ... on DepartureRow {
+              stop {
+                gtfsId
+                code
+                name
+                desc
+              }
+              stoptimes(timeRange: $timeRange, numberOfDepartures: $departuresCount, omitNonPickups: true) {
+                serviceDay
+                scheduledDeparture
+                realtimeDeparture
+                realtimeState
+                realtime
+                headsign
+                trip {
+                  id
+                }
+              }
+              pattern {
+                route {
+                  gtfsId
+                  shortName
+                  longName
+                  mode
+                }
+                code
+                headsign
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
