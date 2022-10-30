eebc65b93c0a4e6793f217efc71883038cc0ec6f
mraatika <raatikaine@gmail.com>
2022-10-30 14:49:31 +0200

    chore: Cypress base url Use baseUrl instead of an environment variable.

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,9 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    env: {
-      APP_URL: 'http://localhost:3000',
-    },
+    baseUrl: 'http://localhost:3000',
     viewportWidth: 375,
     viewportHeight: 812,
     video: false,
