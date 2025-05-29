3b168353464a7e41d3ee9bf18a9a09b08184435b
Miitri Raatikainen <raatikaine@gmail.com>
2025-05-29 20:47:51 +0300

    fix: Check issues

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import './globals.css';
 import App from '@/components/App';
 
 const app = new App({
-  target: document.getElementById('app'),
+  target: document.getElementById('app') as HTMLElement,
 });
 
 export default app;
